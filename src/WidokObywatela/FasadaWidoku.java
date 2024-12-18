d408ab2fceeef95323456f84ad82023d228c6b80
Wojciech Kosierkiewicz <wojtekkosierkiewicz@gmail.com>
2024-12-18 14:06:12 +0100

    ssss

--- src/WidokObywatela/FasadaWidoku.java
+++ src/WidokObywatela/FasadaWidoku.java
@@ -11,28 +11,8 @@ public class FasadaWidoku  {
 	 * @param typWidoku
 	 */
 	public Wniosek getWidok(Wniosek w) {
-		// TODO - implement FasadaWidoku.getWidok
-		if (w.getClass() == WniosekONarodzinyorSmierc.class ) {
-			System.out.println("Wniosek o narodziny lub śmierć");
-			System.out.println("podaj pesel");
-			Scanner scanner = new Scanner(System.in);
-			String pesel = scanner.nextLine();
-			w.setkey("pesel", pesel);
-			System.out.println("podaj datę śmierci");
-			String datasmierci = scanner.nextLine();
-			w.setkey("datasmierci", datasmierci);
-			return w;
-		}else if(w.getClass() == WniosekOAktualizacjeDanych.class){
-			System.out.println("Wniosek o aktualizacje danych");
-			System.out.println("Podaj nowe imie");
-			Scanner scanner = new Scanner(System.in);
-			String noweImie = scanner.nextLine();
-			w.setkey("imie", noweImie);
-			System.out.println("Podaj swoj pesel");
-			String pesel = scanner.nextLine();
-			w.setkey("pesel", pesel);
-			return w;
-		}
+		WidokObywatela widok = new WidokObywatela();
+		widok.pokaz(w);
 		return null;
 	}
 
--- src/WidokObywatela/WidokObywatela.java
+++ src/WidokObywatela/WidokObywatela.java
@@ -2,14 +2,36 @@ package WidokObywatela;
 
 import Prezenter.*;
 
+import java.util.Scanner;
+
 public class WidokObywatela {
 
 	private String typWidoku;
 	private Wniosek wniosek;
 
-	public void pokaz() {
-		// TODO - implement WidokObywatela.pokaz
-		throw new UnsupportedOperationException();
+	public Wniosek pokaz(Wniosek w) {
+		if (w.getClass() == WniosekONarodzinyorSmierc.class ) {
+			System.out.println("Wniosek o narodziny lub śmierć");
+			System.out.println("podaj pesel");
+			Scanner scanner = new Scanner(System.in);
+			String pesel = scanner.nextLine();
+			w.setkey("pesel", pesel);
+			System.out.println("podaj datę śmierci");
+			String datasmierci = scanner.nextLine();
+			w.setkey("datasmierci", datasmierci);
+			return w;
+		}else if(w.getClass() == WniosekOAktualizacjeDanych.class){
+			System.out.println("Wniosek o aktualizacje danych");
+			System.out.println("Podaj nowe imie");
+			Scanner scanner = new Scanner(System.in);
+			String noweImie = scanner.nextLine();
+			w.setkey("imie", noweImie);
+			System.out.println("Podaj swoj pesel");
+			String pesel = scanner.nextLine();
+			w.setkey("pesel", pesel);
+			return w;
+		}
+		return null;
 	}
 
 	public void wyslijwniosek() {
